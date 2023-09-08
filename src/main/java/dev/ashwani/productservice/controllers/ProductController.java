81de583cd370fcc960e0ebe082a688014d88d8c8
ashwani8418 <68030476+ashwani8418@users.noreply.github.com>
2023-09-08 18:11:44 +0530

    product controller and CRUD method implemented

--- src/main/java/dev/ashwani/productservice/controllers/ProductController.java
+++ src/main/java/dev/ashwani/productservice/controllers/ProductController.java
@@ -1,22 +1,33 @@
 package dev.ashwani.productservice.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/products")
 public class ProductController {
 
-    public void createProduct(){
-
-    }
-    public void getAllProducts(){
-
+    @PostMapping()
+    public String createProduct(){
+        return "Creating a product in our database";
     }
 
-    public void getProductById(){
-
+    @GetMapping()
+    public String getAllProducts(){
+        return "Getting all Products Soon";
     }
 
-    public void deleteProductById(){
+    @GetMapping("/{id}")
+    public String getProductById(@PathVariable("id")  Long id){
+        return "Getting product for id " + id;
+    }
 
+    @PutMapping("/{id}")
+    public String updateProductById(@PathVariable("id") Long id){
+        return "Updating product for id " + id;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteProductById(@PathVariable("id") Long id){
+        return "Deleting product for id " + id;
     }
 }
